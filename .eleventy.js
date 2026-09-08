bcee715169a3ba3d9c8505e39d3a8396733f44f4
aubrie <aubriesoucy@gmail.com>
2026-09-07 22:13:48 -0400

    fix eleventy css

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,11 @@
 module.exports = function(eleventyConfig) {
-  // Copy static assets to the final build folder
+  // Pass through CSS and Images directly to output folder
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("images");
-  eleventyConfig.addPassthroughCopy("admin");
 
   return {
-    htmlTemplateEngine: "njk", // Tells Eleventy to parse {{ variables }} in HTML files
+    // Treat HTML files as Nunjucks templates so Front Matter and Liquid/Nunjucks syntax process correctly
+    htmlTemplateEngine: "njk",
     dir: {
       input: ".",
       output: "_site"
